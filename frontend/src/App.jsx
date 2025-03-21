c8d225c6ab22147c0346f6b0796d3721153f2c85
heedu-yoon <dbsgmlen@gmail.com>
2025-03-21 13:17:10 +0900

    mainpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,62 @@
 import "./App.css";
+
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import MainPage from "./page/MainPage/MainPage";
+import About from "./page/About/About";
+import Leadership from "./page/Leadership/Leadership";
+import Board from "./page/Board/Board";
+import Services from "./page/Services/Services";
+import Contact from "./page/Contact/Contact";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
